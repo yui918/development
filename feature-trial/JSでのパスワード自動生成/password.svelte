2873af5e413eecee0940a917ccd2650ead50f664
yui <yuirin13luv@gmail.com>
2023-06-13 00:06:06 +0900

    feature: パスワード自動生成処理を追加

---
+++ feature-trial/JSでのパスワード自動生成/password.svelte
@@ -0,0 +1,17 @@
+<script>
+  let password_base =
+    "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let password;
+  function genPassword(length = 20) {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += password_base.charAt(
+        Math.floor(Math.random() * password_base.length)
+      );
+    }
+    return password;
+  }
+</script>
+
+<button on:click={genPassword}>パスワードを生成する</button>
+<p>{password}</p>
